485678be7c52c24eaebfcfd6c10782b0c0ed898b
codebudy5247 <a.shekhar5247@gmail.com>
2023-11-26 15:02:37 +0530

    user auth docs

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -12,18 +12,84 @@ import { createUserSchema, loginUserSchema } from '../schema/user.schema';
 
 const router = express.Router();
 
-// Register user route
+/**
+   * @openapi
+   * '/api/auth/register':
+   *  post:
+   *     tags:
+   *     - User
+   *     summary: Register a user
+   *     requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *           schema:
+   *              $ref: '#/components/schemas/CreateUserInput'
+   *     responses:
+   *      201:
+   *        description: Success
+   *        content:
+   *          application/json:
+   *            schema:
+   *              $ref: '#/components/schemas/CreateUserResponse'
+   *      409:
+   *        description: Email already exist
+   *      400:
+   *        description: Bad request
+   */
 router.post('/register', validate(createUserSchema), registerHandler);
 
-// Login user route
+
+/**
+   * @openapi
+   * '/api/auth/login':
+   *  post:
+   *     tags:
+   *     - User
+   *     summary: Login a user
+   *     requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *           schema:
+   *              $ref: '#/components/schemas/LoginUserInput'
+   *     responses:
+   *      200:
+   *        description: Success
+   *        content:
+   *          application/json:
+   *            schema:
+   *              $ref: '#/components/schemas/LoginUserResponse'
+   *      401:
+   *        description: Invalid email or password
+   *      409:
+   *        description: Conflict
+   *      400:
+   *        description: Bad request
+   */
+
 router.post('/login', validate(loginUserSchema), loginHandler);
 
-// Refresh access token route
 router.get('/refresh', refreshAccessTokenHandler);
 
 router.use(deserializeUser, requireUser);
 
-// Logout User
+
+/**
+   * @openapi
+   * '/api/auth/logout':
+   *  post:
+   *     tags:
+   *     - User
+   *     summary: Logout a user
+   *     responses:
+   *      200:
+   *        description: Success
+   *      404:
+   *        description: Not Found
+   *      403:
+   *        description: Forbidden
+   */
 router.get('/logout', logoutHandler);
 
 export default router;
--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,5 +1,50 @@
 import { object, string, TypeOf } from 'zod';
 
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    CreateUserInput:
+ *      type: object
+ *      required:
+ *        - email
+ *        - name
+ *        - password
+ *        - passwordConfirm
+ *      properties:
+ *        email:
+ *          type: string
+ *          default: jane.doe@example.com
+ *        name:
+ *          type: string
+ *          default: Jane Doe
+ *        password:
+ *          type: string
+ *          default: stringPassword123
+ *        passwordConfirm:
+ *          type: string
+ *          default: stringPassword123
+ *        avatar:
+ *          type: string
+ *          default: https://mui.com/static/images/avatar/3.jpg
+ *    CreateUserResponse:
+ *      type: object
+ *      properties:
+ *        email:
+ *          type: string
+ *        name:
+ *          type: string
+ *        avatar:
+ *          type: string
+ *        role:
+ *          type: string
+ *        _id:
+ *          type: string
+ *        createdAt:
+ *          type: string
+ *        updatedAt:
+ *          type: string
+ */
 export const createUserSchema = object({
   body: object({
     name: string({ required_error: 'Name is required' }),
@@ -15,6 +60,32 @@ export const createUserSchema = object({
     message: 'Passwords do not match',
   }),
 });
+
+
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    LoginUserInput:
+ *      type: object
+ *      required:
+ *        - email
+ *        - password
+ *      properties:
+ *        email:
+ *          type: string
+ *          default: jane.doe@example.com
+ *        password:
+ *          type: string
+ *          default: stringPassword123
+ *    LoginUserResponse:
+ *      type: object
+ *      properties:
+ *        access_token:
+ *          type: string
+ *        refresh_token:
+ *          type: string
+ */
 
 export const loginUserSchema = object({
   body: object({
