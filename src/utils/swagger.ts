54c6b235136aa9dcddd5d44193f6cde333bddef9
codebudy5247 <a.shekhar5247@gmail.com>
2024-01-25 19:49:40 +0530

    test

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -29,7 +29,7 @@ const options: swaggerJsdoc.Options = {
   };
   
   const swaggerSpec = swaggerJsdoc(options);
-  function swaggerDocs(app: Express, port: any) {
+  function swaggerDocs(app: Express, port: number) {
     // Swagger page
     app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   
