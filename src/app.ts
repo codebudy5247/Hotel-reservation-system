7408d86d4b8f02fc60f064d8c27cf7db088aa2ad
codebudy5247 <a.shekhar5247@gmail.com>
2024-01-25 19:38:25 +0530

    test

--- src/app.ts
+++ src/app.ts
@@ -73,5 +73,5 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
-  connectDB();
+  // connectDB();
 });
